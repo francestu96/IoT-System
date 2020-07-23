80acf3550296c8ac84138e63a2883215dea2e47c
Francesco Stucci <francesco.stucci@horsa.it>
2020-07-23 15:39:57 +0200

    fix BLEScanner app

--- runBackendSimluation.js
+++ runBackendSimluation.js
@@ -5,44 +5,44 @@ var BLEemitter = require("./BackEnd/BLEemitter");
 
 new Server();
 
-// new BLEscanner({
-//     clientId: 'scanner-1',
-//     topic: settings.entranceTopic,
-//     topicDesc: "Entrance at ground floor",
-//     people: 0,
-//     port: 1880
-// });
-// new BLEscanner({
-//     clientId: 'scanner-2',
-//     topic: settings.exitTopic,
-//     topicDesc: "Exit at ground floor",
-//     people: 0,
-//     port: 1881
-// });
-// new BLEscanner({
-//     clientId: 'scanner-3',
-//     topic: settings.firstFloorOfficeTopic,
-//     topicDesc: "Office at first floor",
-//     people: 0,
-//     port: 1882
-// });
-// new BLEscanner({
-//     clientId: 'scanner-4',
-//     topic: settings.firstFloorBathTopic,
-//     topicDesc: "Bathroom at first floor",
-//     people: 0,
-//     port: 1883
-// });
+new BLEscanner({
+    clientId: 'scanner-1',
+    topic: settings.entranceTopic,
+    topicDesc: "Entrance at ground floor",
+    people: 0,
+    port: 1880
+});
+new BLEscanner({
+    clientId: 'scanner-2',
+    topic: settings.exitTopic,
+    topicDesc: "Exit at ground floor",
+    people: 0,
+    port: 1881
+});
+new BLEscanner({
+    clientId: 'scanner-3',
+    topic: settings.firstFloorOfficeTopic,
+    topicDesc: "Office at first floor",
+    people: 0,
+    port: 1882
+});
+new BLEscanner({
+    clientId: 'scanner-4',
+    topic: settings.firstFloorBathTopic,
+    topicDesc: "Bathroom at first floor",
+    people: 0,
+    port: 1883
+});
 
-// new BLEemitter({
-//     clientId: 'emitter-1',
-// });
-// new BLEemitter({
-//     clientId: 'emitter-2',
-// });
-// new BLEemitter({
-//     clientId: 'emitter-3',
-// });
+new BLEemitter({
+    clientId: 'emitter-1',
+});
+new BLEemitter({
+    clientId: 'emitter-2',
+});
+new BLEemitter({
+    clientId: 'emitter-3',
+});
 // new BLEemitter({
 //     clientId: 'emitter-4',
 // });
